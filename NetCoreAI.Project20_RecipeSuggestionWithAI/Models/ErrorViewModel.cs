6eae424281ebf8393775ee907a2a9b08f189e172
bhmberkan <berkanburakturgut@gmail.com>
2025-06-13 23:02:29 +0300

    Revert "RecipeSuggestionWithAI"
    This reverts commit 73348d8c31aef391179b0ca33be2f71b96185fb1.

--- NetCoreAI.Project20_RecipeSuggestionWithAI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NetCoreAI.Project20_RecipeSuggestionWithAI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
